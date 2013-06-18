f68099c856f8cf65bc0c3422a3d27291a5234ae7
Mark Kusch <mark.kusch@gmail.com>
2013-06-18 15:14:21 +0200

    Support all known osfamily
    Use $::osfamily in favor of $::operatingsystem to keep strings
    to match to a minimum possible.
    Provide /etc/fstab for any known Linux based osfamily.

--- manifests/variables.pp
+++ manifests/variables.pp
@@ -1,8 +1,13 @@
 # class fstab::variables
 
 class fstab::variables {
-  $fstab_file = $::operatingsystem ? {
-    /(RedHat|CentOS|Amazon)/ => '/etc/fstab',
-    default                => '/dev/null',
+  $fstab_file = $::osfamily ? {
+    'RedHat'    => '/etc/fstab',
+    'Debian'    => '/etc/fstab',
+    'Suse'      => '/etc/fstab',
+    'Solaris'   => '/etc/fstab',
+    'ArchLinux' => '/etc/fstab',
+    'Mandrake'  => '/etc/fstab',
+    default     => '/dev/null',
   }
 }
